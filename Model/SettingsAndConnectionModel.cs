89569923fff17cd32d45db1dc0d7d1514adaa25a
ArielA147 <43312651+ArielA147@users.noreply.github.com>
2019-04-25 14:34:14 +0300

    Update SettingsAndConnectionModel.cs

--- Model/SettingsAndConnectionModel.cs
+++ Model/SettingsAndConnectionModel.cs
@@ -10,16 +10,7 @@ namespace FlightSimulator
 {
     class SettingsAndConnectionModel
     {
-        //private ClientHandler ch;
         private Server server;
-        //public delegate void updateViewModel(string Lon, string len);
-        //public event updateViewModel FlightBoardData;
-
-        //public LeftModel()
-        //{
-            //Server.Instance.dataEvent += getParametersFromServer;
-        //}
-
         public void ConnectCommand()
         {
             new Task(() =>
@@ -30,9 +21,6 @@ namespace FlightSimulator
 
         public void OpenServer()
         {
-            //ch = new ClientHandler();
-            //ch.dataEvent += getParametersFromServer;
-            //server = new Server(ApplicationSettingsModel.Instance.FlightInfoPort, ApplicationSettingsModel.Instance.FlightServerIP, ch);
             server = Server.Instance;
             server.Start();
         }
@@ -43,8 +31,5 @@ namespace FlightSimulator
             Client.Instance.Close(); // closing the client
         }
 
-        // public void getParametersFromServer(string[] args) {
-        //     FlightBoardData?.Invoke(args[0], args[1]);    
-        // }
     }
 }
